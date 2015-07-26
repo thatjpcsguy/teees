cfd646cca8d87157cb3a56a0452c3963f798aff8
James Cooper-Stanbury <jpcs@freelancer.com>
2015-07-26 14:33:09 +1000

    added other things

---
+++ app.py
@@ -0,0 +1,155 @@
+#/usr/bin/env python2.7
+# from OpenSSL import SSL
+from werkzeug._internal import _log
+
+import logging
+import flask
+import db
+import os
+import forms
+
+
+# Server settings
+host = "0.0.0.0"
+port = 5000
+
+debug = True
+db.engine.echo = True
+
+# SSL parameters
+# context = SSL.Context(SSL.SSLv23_METHOD)
+# context.use_privatekey_file('ssl/flask.pem')
+# context.use_certificate_file('ssl/flask.crt')
+
+# Flask parameters
+SECRET_KEY = "adfklfksdfhlaskdfjhlaskdfhj"  #os.environ['FLASK_SECRET_KEY']
+
+# Recaptcha
+app = flask.Flask(__name__, static_folder='static')
+app.config.from_object(__name__)
+
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Content-Security-Policy', "default-src 'self'; frame-src 'none'; object-src 'none'; reflected-xss 'block'")
+    response.headers.add('X-Permitted-Cross-Domain-Policies', 'master-only')
+    response.headers.add('X-XSS-Protection', '1; mode=block')
+    response.headers.add('X-Frame-Options', 'SAMEORIGIN')
+    response.headers.add('X-Content-Type-Options', 'nosniff')
+    response.headers.add('Cache-Control', 'no-cache, no-store, must-revalidate')
+    response.headers.add('Pragma', 'no-cache')
+    response.headers.add('Expires', '-1')
+    return response
+
+
+@app.route("/", methods=['GET'])
+def index():
+    return flask.render_template("index.html")
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    try:
+        if flask.session['logged_in']:
+            return flask.redirect('/')
+    except KeyError as KE:
+        pass
+
+    error = None
+    form = forms.LoginForm()
+    if flask.request.method == 'POST' and form.validate_on_submit():
+        user_agent = db.clean(str(flask.request.user_agent))
+        username = db.clean(form.username.data)
+        password = db.clean(form.password.data)
+        ip = flask.request.remote_addr
+
+        if db.login(username, password):
+            db.security_log(username, ip, user_agent, action_type='auth_success')
+            flask.session['logged_in'] = True
+            flask.session['username'] = username
+            return flask.redirect('/')
+        else:
+            db.security_log(username, ip, user_agent, action_type='auth_failure')
+            error = "Invalid username/password!"
+
+    elif form.csrf_token.errors:
+        db.security_log(username, ip, user_agent, action_type='csrf')
+        flask.abort(403)
+
+        return flask.redirect('/')
+
+    return flask.render_template("login.html", form=form, error=error)
+
+
+@app.route("/signup", methods=['GET', 'POST'])
+def signup():
+    try:
+        if flask.session['logged_in']:
+            return flask.redirect('/')
+    except KeyError as KE:
+        pass
+
+    errors = None
+    form = forms.RegistrationForm()
+    if flask.request.method == 'POST':
+        if form.validate_on_submit():
+            user_agent = db.clean(str(flask.request.user_agent))
+            username = db.clean(form.username.data)
+            password = db.clean(form.password.data)
+            email = db.clean(form.email.data)
+            ip = flask.request.remote_addr
+
+            if not db.user_exists(username):
+                if db.create_user(username, password, email):
+                    db.security_log(username, ip, user_agent, action_type='account_create')
+            else:
+                _log('warning', "User: %s already exists, cannot create user" % username)
+                error = "Sorry, but that username is taken!"
+
+        else:
+            _log('critical', 'One or more form elements did not pass validation! Errors: %s' % str(form.errors))
+            errors = str(form.errors)
+
+    return flask.render_template("signup.html", form=form, error=errors)
+
+
+@app.route('/logout')
+def logout():
+    try:
+        if not flask.session['logged_in']:
+            return flask.redirect('/')
+    except KeyError as KE:
+        pass
+
+    user_agent = db.clean(str(flask.request.user_agent))
+    username = flask.session['username']
+    ip = flask.request.remote_addr
+
+    db.security_log(username, ip, user_agent, action_type='auth_logout')
+    flask.session.pop('logged_in', None)
+    return flask.redirect('/')
+
+
+@app.route('/robots.txt')
+@app.route('/humans.txt')
+def serve_static():
+    return flask.send_from_directory(app.static_folder, flask.request.path[1:])
+
+
+@app.errorhandler(403)
+def forbidden(error):
+    return flask.render_template("403.html")
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return flask.render_template("404.html")
+
+
+if __name__ == "__main__":
+    logging.basicConfig(filename='app.log', level=logging.DEBUG)
+    # app.run(host=host, debug=debug, port=port, ssl_context=context)
+    app.run(host=host, debug=debug, port=port)
+
+
+
---
+++ db.py
@@ -0,0 +1,134 @@
+#/usr/bin/env python2.7
+from werkzeug._internal import _log
+import sqlalchemy
+import traceback
+import datetime
+import MySQLdb
+import socket
+import struct
+import sys
+import ast
+import os
+
+db_user = 'root'
+db_pass = 'lolol1234' #os.environ['MYSQL_FLASK_GUEST_PW']
+db_host = 'localhost'
+db_db   = 'teees'
+db_port = 3306
+
+user_table = 'users'
+log_table = 'log'
+
+# Connect to DB 
+engine = sqlalchemy.engine.create_engine('mysql://%s:%s@%s/%s' % (db_user, db_pass, db_host, db_db))
+engine.echo = False
+
+# Output connection string
+_log('info', 'Connected to %s on %s:%s as %s' % (db_db, db_host, db_port, db_user))
+
+metadata = sqlalchemy.MetaData(engine)
+users = sqlalchemy.Table(user_table, metadata, autoload=True)
+log = sqlalchemy.Table(log_table, metadata, autoload=True)
+
+def login(username, pw):
+    try:
+        if verify_pw(username, pw) is True:
+            update_sql = users.update().where(users.c.username==username).values(last_login=datetime.datetime.now())
+            engine.execute(update_sql)
+            return True
+    except Exception as E:
+        _log('critical', str(E))
+    return False
+
+def verify_pw(username, pw):
+    try:
+        select_sql = users.select().where(users.c.username==username)
+        rs = engine.execute(select_sql)
+        if rs.rowcount == 1:
+            stored_pw = rs.fetchone()['password']
+            if stored_pw == pw:
+                return True
+    except Exception as E:
+        _log('critical', str(E))
+    return False
+
+def user_exists(username):
+    try:
+        select_sql = users.select(users.c.username).where(users.c.username==username)
+        rs = engine.execute(select_sql)
+        if rs.rowcount != 0:
+            _log('info', 'User: %s exists' % username)
+            return True
+        else:
+            return False
+    except Exception as E:
+        _log('critical', str(E))
+    return False
+
+def create_user(username, password, email):
+    try:
+        insert_sql = users.insert().values(username=username, password=password, email=email)
+        engine.execute(insert_sql)
+    except IntegrityError, err:
+        if err.code==1062:
+            print dir(err)
+
+    except Exception as E:
+        _log('critical', str(E))
+    else:
+        _log('info', 'User: %s created successfully!' % username)
+        return True
+
+def ipv4_to_int(addr):
+    try:
+        socket.inet_aton(addr)
+    except socket.error as SE:
+        _log('critical', '%s (%s)' % (str(SE), addr))
+    else:   
+        return struct.unpack("!I", socket.inet_aton(addr))[0]
+
+def int_to_ipv4(addr):
+    try:
+        socket.inet_ntoa(addr)
+    except struct.error as SE:
+        _log('critical', '%s (%s)' % (str(SE), addr))
+    else:
+        return socket.inet_ntoa(struct.pack("!I", addr))
+
+def clean(arg):
+    """Prepends slashes to \x00, \n, \r, \, ', " and \x1a. literals using MySQLdb.escape_string"""
+    return MySQLdb.escape_string(arg)
+
+def security_log(username, ip, user_agent, action_type):
+    if action_type not in ['account_create', 'auth_success', 'auth_failure', 'auth_logout', 'xss', 'csrf', 'sqli']:
+        _log('warning', 'Invalid action type specified: %s' % action_type)
+    else:
+        if action_type == 'account_create':
+            _log('info', "User %s does not exist, creating user" % username)
+        elif action_type == 'auth_success':
+            _log('info', 'User: %s successfully authenticated (%s)' % (username, ip))
+        elif action_type == 'auth_logout':
+            _log('info', 'User: %s has logged out (%s)' % (username, ip))
+        elif action_type == 'auth_failure':             
+            _log('warning', 'Authentication failed for user: %s (%s)' % (username, ip))
+        elif action_type == 'xss':
+            _log('critical', 'Attempted XSS detected! (%s)' % (ip))
+        elif action_type == 'csrf':
+            _log('critical', 'Attempted CSRF detected! (%s)' % (ip))
+        elif action_type == 'sqli':
+            _log('critical', 'Attempted SQLi detected! (%s)' % (ip))
+
+        # Convert IP address into appropriate format
+        ip_int = ipv4_to_int(ip)
+
+        try:
+            insert_sql = log.insert().values(action_type=action_type, username=username, ip=ip_int, user_agent=user_agent)
+            engine.execute(insert_sql)
+        except Exception as E:
+            print "couldn't log!"
+            _log('critical', str(E))
+        else: 
+            _log('info', 'Logged action: %s by: %s' % (action_type, username))
+
+
+
